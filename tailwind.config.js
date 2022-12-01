d867ac7e06911be8b37e8fb7599682e7e9984b2e
Luis Arrieta <luisarrieta796@gmail.com>
2022-12-01 18:15:41 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+
+    // Container
+    container: {
+      screens: {
+        xl: "1234px",
+      },
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+
+    extend: {
+
+      // Fonts
+      fontFamily: {
+        altair: ['Font-Altair', 'system-ui', 'sans-serif'],
+        futura: ['Font-Futura', 'system-ui', 'sans-serif'],
+      },
+
+      // Palette
+      colors: {
+        'palette-primary': '#009145',
+        'palette-primary-h': '#00B74A',
+        'palette-primary-l': '#007F35',
+
+        'palette-secundary': '#125151',
+        
+        'palette-light': '#F7F9F8',
+        'palette-dark': '#28293D',
+        'palette-dark-h': '#1B1E35',
+        'palette-dark-l': '#363851',
+        
+        'palette-yellow': '#D8DF21',
+        'palette-lima': '#8BC53F',
+        'palette-darkgreen': '#006837',
+        'palette-cyan': '#00A89C',
+        'palette-lightblue': '#29AAE1',
+        'palette-blue': '#0071BB',
+        'palette-indigo': '#2E3191',
+        'palette-darkindigo': '#1B1464',
+      },
+    }
+  },
+  plugins: [],
+};
